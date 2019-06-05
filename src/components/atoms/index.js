3d68419537910c4bcc37a776c96b6db1f377eacf
sbaudray <sebastien.baudray@hetic.net>
2019-06-05 14:52:09 +0200

    active navlink style

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,6 +1,7 @@
 import { Button } from "./button";
+import { NavLink } from "./NavLink";
 import { Title } from "./title";
 import { Select } from "./select";
 import { Subtitle } from "./subtitle";
 
-export { Button, Title, Select, Subtitle };
+export { Button, Title, Select, Subtitle, NavLink };
